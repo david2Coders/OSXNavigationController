2f53018e5b13aff6e8715b803389244f6c824e46
David Santana Molina <david@2coders.com>
2014-11-04 19:44:36 +0000

    Initial commit

---
+++ Source/CODNavigationController.h
@@ -0,0 +1,31 @@
+//
+//  CODNavigationController.h
+//  Sizer
+//
+//  Created by David Santana Molina on 23/10/14.
+//  Copyright (c) 2014 2Coders Studio. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+
+@interface CODNavigationController : NSViewController
+
+@property (nonatomic,strong) CODTitleView * titleView;
+@property (nonatomic,strong) NSView * contentView;
+
+- (instancetype)initWithRootViewController:(NSViewController*)controller;
+- (void)popViewControllerAnimated:(BOOL)animated;
+- (void)pushViewController:(NSViewController*)controller animated:(BOOL)animated;
+
+@end
+
+
+@interface CODBaseViewController : NSViewController
+
+@property (nonatomic,weak) CODNavigationController * navigationController;
+
+
+@end
+
+
